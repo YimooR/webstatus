1a12bdbc4147f08efdac8006dcc26ae22725b8ff
YimooRua <115602418+YimooR@users.noreply.github.com>
2023-03-16 23:31:49 +0800

    Add files via upload

---
+++ build/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'YimooRuaの站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2028803-61f66a361d60ee1c38a18eb4',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://www.yimoorua.top/'
+    },
+    {
+      text: '博客',
+      url: 'https://blog.yimoorua.top/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yimooR/'
+    },
+  ],
+};
